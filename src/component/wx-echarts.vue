9e675fecf49a98a43a7514d0eaa0feb8c3d5da86
hms <hms@123.com>
2018-08-28 14:39:43 +0800

    first commit

---
+++ src/component/wx-echarts.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="vital-chart-wx">
+    <div>wx-echarts</div>
+    <div class="my-charts">
+      <ec-canvas  class="canvas" id="mychart" canvas-id="mychart-bar" :ec="ec"></ec-canvas>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['options'],
+    data () {
+      return {
+        ec: {
+          options: this.options
+        }
+      }
+    },
+    watch: {
+      options: {
+        handler (newValue, oldValue) {
+          this.getOptions(newValue)
+        },
+        deep: true
+      }
+    },
+    created () {
+    },
+    mounted () {
+    },
+    methods: {
+      getOptions: function (newValue) {
+        this.ec.options = newValue
+      }
+    }
+  }
+</script>
+
+<style>
+  .my-charts{
+    height: 500px;
+  }
+  ec-canvas {
+    width: 300px;
+    height: 300px;
+  }
+</style>
---
+++ src/pages/bar/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="counter-warp">
+    <button @click="changeEcharts">切换</button>
+    <wx-echarts :options = 'wxOptions'></wx-echarts>
+    <!--<div class="container">-->
+      <!--<ec-canvas class="canvas" id="mychart-dom-bar" canvas-id="mychart-bar" :ec="ec"></ec-canvas>-->
+    <!--</div>-->
+  </div>
+</template>
+
+<script>
+import wxEcharts from '../../component/wx-echarts'
+
+const data = [
+  [1290, 1330, 1320, 820, 932, 901, 934],
+  [934, 934, 934, 934, 934, 932, 901]
+]
+let i = 0
+
+export default {
+  components: {
+    wxEcharts
+  },
+  data () {
+    return {
+      wxOptions: this.getOptions(0)
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    changeEcharts: function () {
+      if (i) {
+        i = 0
+      } else {
+        i = 1
+      }
+      setTimeout(() => {
+        this.wxOptions = this.getOptions(i)
+      }, 3000)
+    },
+    getOptions: function (i) {
+      let option = {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: data[i],
+          type: 'line'
+        }]
+      }
+      return option
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
